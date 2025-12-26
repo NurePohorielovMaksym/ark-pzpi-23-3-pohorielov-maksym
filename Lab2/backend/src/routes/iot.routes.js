7b7b9322ed750f7bf2a73294d01774ab3d575987
NurePohorielovMaksym <maksym.pohorielov@nure.ua>
2025-12-26 16:55:17 +0200

    Delete Lab2/backend directory

--- Lab2/backend/src/routes/iot.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const iotController = require("../controllers/iot.controller");
-
-// For lab: no auth; in real use apiKey header and validate device
-router.post("/status", iotController.reportStatus);
-
-module.exports = router;
